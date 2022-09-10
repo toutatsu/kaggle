e5f1cec49e3e428b83dc160f2f5a5660f6fcca39
toutatsu <toutatsu.00223+git@gmail.com>
2022-09-10 17:06:29 +0900

    format code

--- Getting_Started/digit-recognizer/misc/PCA.py
+++ Getting_Started/digit-recognizer/misc/PCA.py
@@ -1,39 +1,55 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-import mnist
+# import mnist
 
-def PCA(data,test,r,row=False):
-    print("PCA",flush=True)
-    sample_num=42000
-    test_num=28000
-    d=784
-    if row:data=data.T#データを縦ベクトルに
+
+def PCA(data, test, r, row=False):
+    print("PCA", flush=True)
+    sample_num = 42000
+    test_num = 28000
+    # d=784
+    if row:
+        data = data.T  # データを縦ベクトルに
     # 共分散行列:分散（bias=0）
-    Sigma=np.cov(data,rowvar=1,bias=0)
+    Sigma = np.cov(data, rowvar=1, bias=0)
     eigen_values, eigen_vectors = np.linalg.eig(Sigma)
     # r=2;#写像する空間の次元
-    U=eigen_vectors[:,0:r]#固有値の大きい上位r個の固有ベクトル
-    m=np.mean(data,axis=1)
-    #2次元空間X_に射影
-    data_=np.zeros((r,sample_num))
-    for i in range(1,sample_num):
-        data_[:,i]=U.T@(data[:,i]-m)
-    if row:data_=data_.T
+    U = eigen_vectors[:, 0:r]  # 固有値の大きい上位r個の固有ベクトル
+    m = np.mean(data, axis=1)
+    # 2次元空間X_に射影
+    data_ = np.zeros((r, sample_num))
+    for i in range(1, sample_num):
+        data_[:, i] = U.T@(data[:, i]-m)
+    if row:
+        data_ = data_.T
 
-    if row:test=test.T
-    test_=np.zeros((r,test_num))
-    for i in range(1,test_num):
-        test_[:,i]=U.T@(test[:,i]-m)
-    if row:test_=test_.T
-    return data_,test_
+    if row:
+        test = test.T
+    test_ = np.zeros((r, test_num))
+    for i in range(1, test_num):
+        test_[:, i] = U.T@(test[:, i]-m)
+    if row:
+        test_ = test_.T
+    return data_, test_
 
 
 # digit_data,label,digit_test=mnist.data_load()
 
 # digit_data_,digit_test_=PCA(digit_data,digit_test,2)
-# #digit_color= {'0':"black",'1':"red",'2':"orange",'3':"gold",'4':"lime",'5':"blue",'6':"green",'7':"cyan",'8':"blue",'9':"magenta"}
-# digit_color= {0:"black",1:"red",2:"orange",3:"gold",4:"lime",5:"blue",6:"green",7:"cyan",8:"blue",9:"magenta"}
+
+# digit_color = {
+#     0: "black",
+#     1: "red",
+#     2: "orange",
+#     3: "gold",
+#     4: "lime",
+#     5: "blue",
+#     6: "green",
+#     7: "cyan",
+#     8: "blue",
+#     9: "magenta"
+# }
 
 # plt.title("PCA")
 # for i in range(0,3000):
--- Getting_Started/digit-recognizer/misc/mnist.py
+++ Getting_Started/digit-recognizer/misc/mnist.py
@@ -1,40 +1,76 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sample_num=42000
-test_num=28000
-d=784
+sample_num = 42000
+test_num = 28000
+d = 784
+
 
 def data_load():
-    print("train data loading...",flush=True,end="")
-    digit_data=np.loadtxt("train.csv",dtype=int,delimiter=",",skiprows=1,max_rows=sample_num,usecols=np.arange(1,d+1))
-    label=np.loadtxt("train.csv",dtype=int,delimiter=",",skiprows=1,max_rows=sample_num,usecols=(0))
-    print("loaded",flush=True)
+    print("train data loading...", flush=True, end="")
+    digit_data = np.loadtxt(
+        "train.csv",
+        dtype=int,
+        delimiter=",",
+        skiprows=1,
+        max_rows=sample_num,
+        usecols=np.arange(1, d+1)
+    )
+    label = np.loadtxt(
+        "train.csv",
+        dtype=int,
+        delimiter=",",
+        skiprows=1,
+        max_rows=sample_num,
+        usecols=(0)
+    )
+    print("loaded", flush=True)
+    print("test data loading...", flush=True, end="")
+    digit_test = np.loadtxt("test.csv", dtype=int, delimiter=",", skiprows=1)
+    print("loaded", flush=True)
 
-    print("test data loading...",flush=True,end="")
-    digit_test=np.loadtxt("test.csv",dtype=int,delimiter=",",skiprows=1)
-    print("loaded",flush=True)
+    return digit_data, label, digit_test
 
-    return digit_data,label,digit_test
 
-#https://stackoverflow.com/questions/37228371/visualize-mnist-dataset-using-opencv-or-matplotlib-pyplot
-def digit_visualize(digit_data,height=28,width=28,label=None):
+# https://stackoverflow.com/questions/37228371/visualize-mnist-dataset-using-opencv-or-matplotlib-pyplot
+def digit_visualize(digit_data, height=28, width=28, label=None):
     plt.title('digit:{}'.format(label))
-    plt.imshow(np.array(digit_data, dtype='uint8').reshape((height, width)), cmap='gray')
+    plt.imshow(
+        np.array(digit_data, dtype='uint8').reshape((height, width)),
+        cmap='gray'
+    )
     plt.show()
 
-def write_prediction(file_name,predictions):
+
+def write_prediction(file_name, predictions):
     with open(file_name, mode='w') as f:
         f.write("ImageId,Label\n")
-        for i in range(0,test_num):
-            print("{}".format(i),flush=True)
-            f.write("{},{}\n".format(i+1,predictions[i]))
+        for i in range(0, test_num):
+            print("{}".format(i), flush=True)
+            f.write("{},{}\n".format(i+1, predictions[i]))
 
-def visualize_2d(data,label,plot_name="digit",num=1000):
-    digit_color= {0:"black",1:"red",2:"orange",3:"gold",4:"lime",5:"blue",6:"green",7:"cyan",8:"blue",9:"magenta"}
+
+def visualize_2d(data, label, plot_name="digit", num=1000):
+    digit_color = {
+        0: "black",
+        1: "red",
+        2: "orange",
+        3: "gold",
+        4: "lime",
+        5: "blue",
+        6: "green",
+        7: "cyan",
+        8: "blue",
+        9: "magenta"
+    }
     plt.title(plot_name)
-    for i in range(0,num):
+    for i in range(0, num):
         print(i)
         print(label[i])
-        plt.scatter(data[i,0],data[i,1],color=digit_color[label[i]],marker="$"+str(label[i])+"$")
+        plt.scatter(
+            data[i, 0],
+            data[i, 1],
+            color=digit_color[label[i]],
+            marker="$"+str(label[i])+"$"
+        )
     plt.show()
